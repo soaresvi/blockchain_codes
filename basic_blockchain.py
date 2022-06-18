3dbbc8c80b7c98ca6869cb6f03703ae2a83d5e70
soaresvi <61430270+soaresvi@users.noreply.github.com>
2022-06-18 19:48:00 +0200

    Update basic_blockchain.py

--- basic_blockchain.py
+++ basic_blockchain.py
@@ -1,6 +1,7 @@
 import datetime as d
 import hashlib as h
-from sqlite3 import Timestamp
+#from sqlite3 import Timestamp
+
 
 class Block:
     def __init__(self, index, timestamp, data, prevhash):
@@ -13,31 +14,34 @@ class Block:
 
     def hashblock(self):
         block_encryption = h.sha256()
-        block_encryption.update(str(self.index)+str(self.timestamp)+str(self.data)+str(self.prevhash))
+        assemble = bytes((str(self.index) + str(self.timestamp) + str(self.data) + str(self.prevhash)).encode('utf-8'))
+        block_encryption.update(assemble)
         return block_encryption.hexdigest()
-    
+
     @staticmethod
     def genesisblock():
         return Block(0, d.datetime.now(), "genesis block trabsaction", " ")
 
     @staticmethod
     def newBlock(lastblock):
-        index = lastblock.index+1
+        index = lastblock.index + 1
         timestamp = d.datetime.now()
         hashblock = lastblock.hash
-        data = "Transaction "+str(index)
+        data = "Transaction " + str(index)
         return Block(index, timestamp, data, hashblock)
+
 
 blockchain = [Block.genesisblock()]
 prevblock = blockchain[0]
 
-for i in range (0,5):
-    addblock = Block.newblock(prevblock)
+for i in range(0, 5):
+    addblock = Block.newBlock(prevblock)
     blockchain.append(addblock)
-    prevblock =addblock
+    prevblock = addblock
 
     print("Block ID {} ".format(addblock.index))
     print("Timestamp:{}".format(addblock.timestamp))
     print("Hash of the block:{}".format(addblock.hash))
     print("Previous Block Hash:{}".format(addblock.prevhash))
     print("data:{}\n".format(addblock.data))
+
